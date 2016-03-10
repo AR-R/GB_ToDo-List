4b68348fe384b1b673e5f3e8ad13409b0f18ebcd
Андрей Ремизов <andrejremizov@macbook-pro-andrey.local>
2016-03-11 00:12:35 +0300

    // Delete some notification of the tableLocalNotification and the required position at arrayEvent

--- ToDo List/MainTableViewController.m
+++ ToDo List/MainTableViewController.m
@@ -127,13 +127,13 @@
 
 
 
-/*
+
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
+
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -163,17 +163,29 @@
     
 }
 
-/*
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
+        // Delete the row from the data source (how abolish and release the some event)
+        
+          UILocalNotification * notification = [self.arrayEvents objectAtIndex:indexPath.row];
+        
+        // Delete some notification of the tableLocalNotification
+        
+        [[UIApplication sharedApplication] cancelLocalNotification:notification];
+        
+        // Delete the required position from arrayEvents
+        
+        [self.arrayEvents removeObjectAtIndex:indexPath.row];
+        
+        
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.
