021962596fa5cf6b32167523e8f67fb881fcfd35
Андрей Ремизов <andrejremizov@macbook-pro-andrey.local>
2016-03-11 00:16:53 +0300

    Remove blocks with places

--- ToDo List/MainTableViewController.m
+++ ToDo List/MainTableViewController.m
@@ -134,6 +134,28 @@
     return YES;
 }
 
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source (how abolish and release the some event)
+        
+        UILocalNotification * notification = [self.arrayEvents objectAtIndex:indexPath.row];
+        
+        // Delete some notification of the tableLocalNotification
+        
+        [[UIApplication sharedApplication] cancelLocalNotification:notification];
+        
+        // Delete the required position from arrayEvents
+        
+        [self.arrayEvents removeObjectAtIndex:indexPath.row];
+        
+        
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+}
+
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
@@ -164,27 +186,6 @@
 }
 
 
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source (how abolish and release the some event)
-        
-          UILocalNotification * notification = [self.arrayEvents objectAtIndex:indexPath.row];
-        
-        // Delete some notification of the tableLocalNotification
-        
-        [[UIApplication sharedApplication] cancelLocalNotification:notification];
-        
-        // Delete the required position from arrayEvents
-        
-        [self.arrayEvents removeObjectAtIndex:indexPath.row];
-        
-        
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
 
 
 /*
